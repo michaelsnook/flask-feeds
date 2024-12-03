4b1fe4def56f4ead4639b246b71e919996435e7b
em snook <michael.snook@gmail.com>
2024-12-03 18:36:01 +0530

    Add hello-world response

--- api/index.py
+++ api/index.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
+@app.route('/feeds/indiasky')
+def home():
+    return {"hello": "world"}
+
+
+@app.route('/feeds/about')
 def about():
     return 'About'
